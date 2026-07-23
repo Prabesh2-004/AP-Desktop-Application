f71a343c82c44f4e8cb7aa51c9ae809fa2aa99b5
Prabesh2-004 <coder.bro2004@gmail.com>
2026-07-23 22:14:09 +0545

    resources page added

--- src/main/java/com/project/apdesktopapplication/Login.java
+++ src/main/java/com/project/apdesktopapplication/Login.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Login extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("loginPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Login");
         stage.setScene(scene);
