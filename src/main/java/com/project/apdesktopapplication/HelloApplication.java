34d189dc33e30ec60f8f845ea82052fb1f215417
Pratham943 <phoneixwasfound69@gmail.com>
2026-07-23 21:57:15 +0545

    student panel and staff panel created

--- src/main/java/com/project/apdesktopapplication/HelloApplication.java
+++ src/main/java/com/project/apdesktopapplication/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Login");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("StudentManagment.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1400, 850);
+        stage.setTitle("Student Dashboard");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/project/apdesktopapplication/StaffController.java
@@ -0,0 +1,22 @@
+package com.project.apdesktopapplication;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class StaffController {
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private TextField searchField;
+
+    @FXML
+    private TableView<?> resourceTable;
+
+    @FXML
+    private TableView<?> bookingTable;
+
+}
